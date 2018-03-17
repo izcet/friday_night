7c0853a88feba49e38c0218185c83a1f0c8ce0e0
Isaac Rhett <isaacjrhett@gmail.com>
2018-03-16 23:51:14 -0700

    [what_should_i_do_tonight.sh] [data/meta -> data/.meta]

--- what_should_i_do_tonight.sh
+++ what_should_i_do_tonight.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+
+
+################################################################################
 ## USER VARIABLES ##
 
 # Remove or set a file hidden to exclude it from the list
@@ -10,15 +13,16 @@
 #   activity1
 #   #activity2
 
-# Increase this to have longer sentences
-NUM_THINGS=2
+NUM_THINGS=2  # Increase this to have longer sentences
 
-# Set this to 0 to remove the meta effects
-USE_META=1
+USE_META=1  # Set this to 0 to remove the meta effects
 
-# Set to 1 to add duplicate lines (and skew the odds)
-ALLOW_DUPE=0
+ALLOW_DUPE=0  # Set to 1 to add duplicate lines (and skew the odds)
 
+
+
+
+################################################################################
 ## CONSTANTS ##
 
 REL_PATH="$( cd "$(dirname "$0")" ; pwd -P )"
@@ -26,6 +30,9 @@ DATA_DIR="data"
 DATA="$REL_PATH/$DATA_DIR"
 NUM_FILES=$(ls -1 $DATA | wc -l)
 
+
+
+################################################################################
 ## ERROR CHECKS ##
 
 if [ "$NUM_FILES" -eq "0" ] ; then
@@ -34,71 +41,44 @@ if [ "$NUM_FILES" -eq "0" ] ; then
 fi
 
 
+
+################################################################################
 ## FUNCTION DEFINITIONS
 
-function get_file () {
-    INDEX=$(($(($RANDOM % $NUM_FILES)) + 1))
-    FILE=$(ls -1 $DATA | head -n $INDEX | tail -n 1
+function get_rand () {
+    echo "$(($(($RANDOM % $1)) + 1))"
+}
 
-   echo "$FILE"
+function get_file () {
+    INDEX="$(get_rand "$NUM_FILES")"
+    FILE=$(ls -1 $DATA | head -n $INDEX | tail -n 1 )
+
+    echo "$FILE"
 }
 
 function get_line () {
-    FILE=$1
+    FILE=$DATA/$1
     WORDS="$(cat $FILE | grep -v "^\s*#")"
     if [ "$ALLOW_DUPE" -eq "0" ] ; then
-        WORDS="$(echo "$WORDS" | sort | uniq)"
+        WORDS="$(echo "$WORDS" | sed 's/ /\n/g' | sort | uniq)"
     fi
-    NUM_WORDS=$(echo $WORDS | 
-
-
+    NUM_WORDS=$(echo $WORDS | sed 's/ /\n/g' | wc -l)
+    INDEX="$(get_rand $NUM_WORDS)"
+    echo "$(cat "$FILE" | head -n "$INDEX" | tail -n 1)"
 }
 
+
+
+################################################################################
 ## CODE ##
 
+if [ "$1" == "bulk" ] ; then
+    I=0
+    while [ "$I" -lt "9" ] ; do
+        echo "$(get_line $(get_file)) and $(get_line $(get_file))"
+        I=$(($I + 1))
+    done
+fi
 
+echo "$(get_line $(get_file)) and $(get_line $(get_file))"
 
-
-function getLine () {
-	DOC=$1
-	LEN=$(wc -l $DOC | sed 's/ *//' | cut -d' ' -f1)
-	RND=$(($(($RANDOM % $LEN)) + 1))
-	if [ $# -gt 1 ] ; then
-		if [ $(($RND % 2)) -eq 0 ] ; then
-			END="?"
-		else
-			END="."
-		fi
-		echo -n "$END#"
-	fi
-	TXT=$(head -n $RND $DOC | tail -1)
-	echo $TXT
-}
-
-
-SRCS=txts
-OPEN="1_opener.txt"
-PLAT="2_platform.txt"
-TRAN="3_transition.txt"
-VERB="4_action.txt"
-SUBJ="5_subject.txt"
-
-UNO=$SRCS/$OPEN
-DOS=$SRCS/$PLAT
-TRE=$SRCS/$TRAN
-QUA=$SRCS/$VERB
-CIN=$SRCS/$SUBJ
-
-END=""
-
-
-UNO=$(getLine $UNO "1")
-DOS=$(getLine $DOS)
-TRE=$(getLine $TRE)
-QUA=$(getLine $QUA)
-CIN=$(getLine $CIN)
-
-END=$(echo $UNO | cut -d'#' -f1)
-UNO=$(echo $UNO | cut -d'#' -f2-)
-
-echo "$UNO $DOS $TRE $QUA $CIN$END"
